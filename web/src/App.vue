4ab79696746c91717fef35036744bffc46edf0fb
coderjc <coderjc@qq.com>
2026-03-06 17:12:24 +0800

    Initialize Vue 3 + TypeScript + Vite project with essential files and configurations

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="app">hello word</main>
+</template>
+
+<style scoped lang="scss">
+.app {
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    font-size: 32px;
+    font-weight: 600;
+}
+</style>
