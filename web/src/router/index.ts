1440f37e50c1041120ad0ee6b4bde147afa95973
coderjc <coderjc@qq.com>
2026-03-18 15:39:59 +0800

    feat(web): 新增搜索页面并更新路由配置
    - 在路由中新增搜索页面的路径和组件
    - 创建搜索页面组件，包含基本的模板和样式

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'bookshelf',
-      component: () => import('@/views/bookshelf/index.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('@/views/not-found/index.vue'),
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'bookshelf',
+            component: () => import('@/views/bookshelf/index.vue'),
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: () => import('@/views/search/index.vue'),
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: () => import('@/views/not-found/index.vue'),
+        },
+    ],
 })
 
 export default router
